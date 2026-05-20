ec6bf63f567e8406839b04b42327ad6d207ef64d
ops-shepherds <ops@shepherds.com>
2026-05-20 12:12:35 -0500

    Status Updates index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
